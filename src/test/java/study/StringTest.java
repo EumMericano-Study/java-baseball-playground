436ced4f14183b820ba7aa2dc5176d1b8f998c9d
Eumericano <opwer032@naver.com>
2024-03-11 07:14:12 +0900

    feat: 요구사항 1 저장

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,7 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
     @Test
     void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
+        String testWord = "abc".replace("b", "d");
+        assertThat(testWord).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] splitList = "1,2".split(",");
+        assertThat(splitList).containsExactly("1","2");
+
+        String[] testWords = "1".split(",");
+        assertThat(testWords).contains("1").hasSize(1);
+
     }
 }
