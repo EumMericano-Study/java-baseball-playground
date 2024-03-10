ae4e80455dbb442889766e352c8b45b9027a953e
Eumericano <opwer032@naver.com>
2024-03-11 07:22:05 +0900

    test: substring 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -6,18 +6,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
-    void replace() {
+    void replace_학습_테스트() {
         String testWord = "abc".replace("b", "d");
         assertThat(testWord).isEqualTo("adc");
     }
 
     @Test
-    void split() {
+    void split_학습_테스트() {
         String[] splitList = "1,2".split(",");
         assertThat(splitList).containsExactly("1","2");
 
         String[] testWords = "1".split(",");
         assertThat(testWords).contains("1").hasSize(1);
+    }
 
+    @Test
+    void substring_학습_테스트() {
+        String testWord = "(1,2)";
+        String subStringWord = testWord.substring(1, testWord.length()-1);
+        assertThat(subStringWord).isEqualTo("1,2");
     }
 }
